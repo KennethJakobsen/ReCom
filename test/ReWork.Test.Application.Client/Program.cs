02f555527bdd8ba9e4e7f0d0d508c822e01f2475
Kenneth Jakobsen <kenneth.jakobsen@yahoo.com>
2018-07-13 08:58:55 +0200

    Changed testing

--- test/ReWork.Test.Application.Client/Program.cs
+++ test/ReWork.Test.Application.Client/Program.cs
@@ -15,11 +15,6 @@ namespace ReWork.Test.Application.Client
     {
         public static void Main()
         {
-            //var act = new DefaultActivator();
-            //act.Register<ICommand<HelloWorldCommand>, HelloWorldCommandHandler>();
-            //act.Register<IActivator>(act);
-            //var factory = new HandlerDispatcher(act);
-            //factory.Execute(new HelloWorldCommand() {Message = "test"}).Wait();
             Connect("127.0.0.1", "test");
         }
         static void Connect(String server, String message)
@@ -27,10 +22,7 @@ namespace ReWork.Test.Application.Client
             try
             {
                 Thread.Sleep(1000);
-                // Create a TcpClient.
-                // Note, for this client to work you need to have a TcpServer 
-                // connected to the same address as specified by the server, port
-                // combination.
+                
                 var activator = new DefaultActivator();
                 activator.Register<ICommand<PingPongCommand>, PingPongCommandHandler>();
                 activator.Register<ICommand<WelcomeMessage>, WelcomeMessageHandler>();
@@ -45,40 +37,6 @@ namespace ReWork.Test.Application.Client
                 }
                 
 
-                //// Translate the passed message into ASCII and store it as a Byte array.
-                //Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
-
-                //// Get a client stream for reading and writing.
-                ////  Stream stream = client.GetStream();
-
-                //NetworkStream stream = client.GetStream();
-                //while (true)
-                //{
-                //    // Send the message to the connected TcpServer. 
-                //    stream.Write(data, 0, data.Length);
-
-                //    Console.WriteLine("Sent: {0}", message);
-
-                //    // Receive the TcpServer.response.
-
-                //    // Buffer to store the response var .
-                //    data = new Byte[256];
-
-                //    // String to store the response ASCII representation.
-                //    String responseData = String.Empty;
-
-                //    // Read the first batch of the TcpServer response bytes.
-                //    Int32 bytes = stream.Read(data, 0, data.Length);
-                //    responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-                //    Console.WriteLine("Received: {0}", responseData);
-
-                //    Thread.Sleep(2000);
-                //}
-
-
-                //// Close everything.
-                //stream.Close();
-                //client.Close();
             }
             catch (ArgumentNullException e)
             {
--- test/ReWork.Test.Common.Domain/PingPongCommand.cs
+++ test/ReWork.Test.Common.Domain/PingPongCommand.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ReWork.Test.Common.Domain
+﻿namespace ReWork.Test.Common.Domain
 {
     public  class PingPongCommand
     {
